d79a43b60b39b5e7b3dffb365ef880b8047d9ee3
Naran Babha <nab115@pitt.edu>
2023-02-20 00:20:53 -0800

    adding scraper api

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import os
 
 from utils import get_db_client
 from match import get_matching_items
+from menu_scraper import create_restaurant_object
 
 env = os.environ.get('PYTHON_ENV')
 app = Flask(__name__)
@@ -53,6 +54,24 @@ def location_search():
 
     restaurants = col.find({'city': city}, {'_id': False})
     return [r for r in restaurants]
+
+@app.route('/scraper', methods=['POST'])
+def scraper():
+    db = client['Restaurants']
+    col = db['restaurants']
+
+    try:
+        url = request.json['url']
+        try:
+            restaurant = create_restaurant_object(url, url, '', '')
+            return restaurant
+        except(e):
+            print(url + ' did not work')
+            print(e.message)
+    except:
+        print('Bad Request : url not sent')
+    
+    return {}
 
 if __name__ == '__main__':
     if (env == 'development'):
